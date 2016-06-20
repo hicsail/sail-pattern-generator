fc08dae25b2a7f04891d9bc9c86308b74bc795dd
Frederick Jansen <frederick.jansen@gmail.com>
2016-06-20 17:08:52 -0400

    Early SVG generate test, messes up some colors

--- js/generator.js
+++ js/generator.js
@@ -12,21 +12,34 @@ $(function () {
       var numVer = gridSizeY * 4;
       var triangleWidth = patternSize / 4;
 
+      var polys = [];
+
       ctx.canvas.width = patternSize * gridSizeX;
       ctx.canvas.height = patternSize * gridSizeY;
 
-      for (var i = 0; i < numHor; i++) {
-        for (var j = 0; j < numVer; j++) {
+      for (var j = 0; j < numHor; j++) {
+        for (var i = 0; i < numVer; i++) {
           var rotated = Math.round(Math.random());
+          var offsetX = i * triangleWidth;
+          var offsetY = j * triangleWidth;
+          var color = randomColor();
 
-          drawTriangle(ctx, i * triangleWidth, j * triangleWidth, triangleWidth, randomColor(), false, rotated);
-          drawTriangle(ctx, i * triangleWidth, j * triangleWidth, triangleWidth, randomColor(), true, rotated);
+
+          drawTriangle(ctx, offsetX, offsetY, triangleWidth, color, false, rotated);
+          polys.push(buildPoly(offsetX, offsetY, triangleWidth, color, false, rotated));
+
+          // Draw second part of square
+          color = randomColor();
+          drawTriangle(ctx, offsetX, offsetY, triangleWidth, randomColor(), true, rotated);
+          polys.push(buildPoly(offsetX, offsetY, triangleWidth, color, true, rotated));
         }
       }
     }
+    return polys;
   }
 
   function drawTriangle(ctx, offsetX, offsetY, width, color, flipped, rotated) {
+
     ctx.beginPath();
     ctx.fillStyle = color;
     if (!flipped) {
@@ -58,9 +71,52 @@ $(function () {
     ctx.fill();
   }
 
+  function buildPoly(offsetX, offsetY, width, color, flipped, rotated) {
+    var points = [[offsetX, offsetY], [offsetX + width, offsetY], [offsetX, offsetY + width]];
+    var pointsStr = [];
+
+    for (var i = 0; i < points.length; i++) {
+      // Move to origin to rotate
+      points[i][0] -= offsetX + (width / 2);
+      points[i][1] -= offsetY + (width / 2);
+
+      if (!flipped) {
+        if (rotated) {
+          // Rotate 270 degrees
+          [points[i][0], points[i][1]] = [points[i][1], -points[i][0]]
+        } else {
+          // Do nothing, default form
+        }
+      } else {
+        if (rotated) {
+          // Rotate 90 degrees
+          [points[i][0], points[i][1]] = [-points[i][1], points[i][0]]
+        } else {
+          // Rotate 180 degrees
+          [points[i][0], points[i][1]] = [-points[i][0], -points[i][1]]
+        }
+      }
+      points[i][0] += offsetX + (width / 2);
+      points[i][1] += offsetY + (width / 2);
+
+      pointsStr.push(points[i][0].toString() + ',' + points[i][1])
+    }
+
+    return '<polygon fill="' + color + '" points="' + pointsStr.join(' ') + '"></polygon>';
+  }
+
   function randomColor() {
     return colors[Math.round(Math.random() * (colors.length - 1))]
   }
 
-  draw();
+  function addSVGHeader(polys) {
+    var svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg" x="0" y="0" >';
+    svg += polys.join('');
+    svg += '</svg>';
+    return svg;
+  }
+
+  var polys = draw();
+  var svg = addSVGHeader(polys);
+  console.log(svg.toString());
 });
