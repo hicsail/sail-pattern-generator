ae9f546b1f8166b421e0c06b2652681c7bab09f1
Frederick Jansen <frederick.jansen@gmail.com>
2016-06-21 11:54:46 -0400

    Use rotation instead of manually calculating triangle points Will come in handy when rotating individual objects

--- js/generator.js
+++ js/generator.js
@@ -21,65 +21,75 @@ $(function () {
       for (var j = 0; j < numHor; j++) {
         for (var i = 0; i < numVer; i++) {
           var rotated = Math.round(Math.random());
-          var offsetX = i * triangleWidth;
-          var offsetY = j * triangleWidth;
+          var x = i * triangleWidth;
+          var y = j * triangleWidth;
           var color = randomColor();
 
 
-          drawTriangle(ctx, offsetX, offsetY, triangleWidth, color, false, rotated);
-          polys.push(buildPoly(offsetX, offsetY, triangleWidth, color, false, rotated));
+          drawTriangle(ctx, x, y, triangleWidth, color, false, rotated);
+          polys.push(buildPoly(x, y, triangleWidth, color, false, rotated));
 
           // Draw second part of square
           color = randomColor();
-          drawTriangle(ctx, offsetX, offsetY, triangleWidth, color, true, rotated);
-          polys.push(buildPoly(offsetX, offsetY, triangleWidth, color, true, rotated));
+          drawTriangle(ctx, x, y, triangleWidth, color, true, rotated);
+          polys.push(buildPoly(x, y, triangleWidth, color, true, rotated));
         }
       }
     }
     return polys;
   }
 
-  function drawTriangle(ctx, offsetX, offsetY, width, color, flipped, rotated) {
+  function drawTriangle(ctx, x, y, width, color, flipped, rotated) {
+    var rad;
 
-    ctx.beginPath();
-    ctx.fillStyle = color;
+    ctx.save();
+    // Move origin for rotation
+    ctx.translate(x + width / 2, y + width / 2);
+
+    // Define rotation
     if (!flipped) {
       if (rotated) {
         // ◺
-        ctx.moveTo(offsetX, offsetY + width);
-        ctx.lineTo(offsetX, offsetY);
-        ctx.lineTo(offsetX + width, offsetY + width);
+        rad = 270 * 3.14159265 / 180;
         // ◸
       } else {
-        ctx.moveTo(offsetX, offsetY);
-        ctx.lineTo(offsetX + width, offsetY);
-        ctx.lineTo(offsetX, offsetY + width);
+        rad = 0;
       }
     } else {
       if (rotated) {
         // ◹
-        ctx.moveTo(offsetX + width, offsetY);
-        ctx.lineTo(offsetX + width, offsetY + width);
-        ctx.lineTo(offsetX, offsetY);
+        rad = 90 * 3.14159265 / 180;
       } else {
         // ◿
-        ctx.moveTo(offsetX + width, offsetY + width);
-        ctx.lineTo(offsetX, offsetY + width);
-        ctx.lineTo(offsetX + width, offsetY);
+        rad = 3.14159265;
       }
     }
 
+    // Rotate and restore canvas position
+    ctx.rotate(rad);
+    ctx.translate(-(x + width / 2), -(y + width / 2));
+
+    // Draw triangle
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + width, y);
+    ctx.lineTo(x, y + width);
     ctx.fill();
+
+    // Restore canvas
+    ctx.restore();
+
   }
 
-  function buildPoly(offsetX, offsetY, width, color, flipped, rotated) {
-    var points = [[offsetX, offsetY], [offsetX + width, offsetY], [offsetX, offsetY + width]];
+  function buildPoly(x, y, width, color, flipped, rotated) {
+    var points = [[x, y], [x + width, y], [x, y + width]];
     var pointsStr = [];
 
     for (var i = 0; i < points.length; i++) {
       // Move to origin to rotate
-      points[i][0] -= offsetX + (width / 2);
-      points[i][1] -= offsetY + (width / 2);
+      points[i][0] -= x + width / 2;
+      points[i][1] -= y + width / 2;
 
       if (!flipped) {
         if (rotated) {
@@ -97,8 +107,8 @@ $(function () {
           [points[i][0], points[i][1]] = [-points[i][0], -points[i][1]]
         }
       }
-      points[i][0] += offsetX + (width / 2);
-      points[i][1] += offsetY + (width / 2);
+      points[i][0] += x + width / 2;
+      points[i][1] += y + width / 2;
 
       pointsStr.push(points[i][0].toString() + ',' + points[i][1])
     }
