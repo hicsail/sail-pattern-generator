0277b6b3d3ef9a4922338206adbafc57994d6512
Frederick Jansen <frederick.jansen@gmail.com>
2016-07-17 15:19:02 -0400

    Undo +1 px hack in favor of crispEdges shape-rendering Objects now rotate correctly

--- js/generator.js
+++ js/generator.js
@@ -36,7 +36,8 @@ $(function () {
       .html('')
       .append('svg')
       .attr('width', patternSize * gridSizeX)
-      .attr('height', patternSize * gridSizeY);
+      .attr('height', patternSize * gridSizeY)
+      .attr('shape-rendering', 'crispEdges');
 
     svg.on('click', function () {
       if (d3.event.ctrlKey) {
@@ -168,13 +169,7 @@ $(function () {
   }
 
   function getSVGPoints(x, y, width, flipped, rotated) {
-    var points;
-    // Add 1 pixel overlap to first of 2 triangles to fix aliasing issue
-    if (!flipped) {
-      points = [[x, y], [x + width, y], [x, y + width]];
-    } else {
-      points = [[x, y], [x + width, y], [x, y + width]];
-    }
+    var points = [[x, y], [x + width, y], [x, y + width]];
 
     for (var i = 0; i < points.length; i++) {
       // Move to origin to rotate
@@ -197,6 +192,7 @@ $(function () {
           [points[i][0], points[i][1]] = [-points[i][0], -points[i][1]]
         }
       }
+
       points[i][0] += x + width / 2;
       points[i][1] += y + width / 2;
     }
@@ -227,28 +223,19 @@ $(function () {
 
   function rotateTriangle(target) {
     var currentTriangle = d3.select(target),
-      thisPoints = stringToPoints(currentTriangle.attr('points')),
-      firstElement = false;
+      thisPoints = stringToPoints(currentTriangle.attr('points'));
 
     var otherTriangle = d3.select(target.previousElementSibling);
     if (otherTriangle.empty()) {
       otherTriangle = d3.select(target.nextElementSibling);
-      firstElement = true;
     }
     var otherPoints = stringToPoints(otherTriangle.attr('points'));
 
     var width = Math.max(Math.abs(thisPoints[0][0] - thisPoints[1][0]), Math.abs(thisPoints[0][0] - thisPoints[2][0])),
       x, y;
 
-    if (firstElement) {
-      x = Math.min(thisPoints[0][0], thisPoints[1][0], thisPoints[2][0]);
-      y = Math.min(thisPoints[0][1], thisPoints[1][1], thisPoints[2][1]);
-    } else {
-      x = Math.min(otherPoints[0][0], otherPoints[1][0], otherPoints[2][0]);
-      y = Math.min(otherPoints[0][1], otherPoints[1][1], otherPoints[2][1]);
-    }
-
-    console.log(x, y);
+    x = Math.min(thisPoints[0][0], thisPoints[1][0], thisPoints[2][0]);
+    y = Math.min(thisPoints[0][1], thisPoints[1][1], thisPoints[2][1]);
 
     for (var i = 0; i < thisPoints.length; i++) {
       thisPoints[i][0] -= x + width / 2;
@@ -292,7 +279,7 @@ $(function () {
   }
 
   function addSVGHeader(polys) {
-    var svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg" x="0" y="0" >\n';
+    var svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg" x="0" y="0" shape-rendering="crispEdges" >\n';
     svg += polys.join('\n');
     svg += '\n</svg>';
     return svg;
